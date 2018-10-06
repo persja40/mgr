0159188fbeba8c895c16c2a433a99037ee0c20f6
persja40 <persja40@gmail.com>
2018-10-06 18:27:42 +0200

    help

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
-#include "read_iris.h"
-
 #include <vector>
-#include "kernel.h"
-using namespace std;
+#include <cuda_runtime.h>
+#include <cuda.h>
+
+#include "read_iris.h"
+#include "kernel.cu"
 
 int main(int argc, char **argv)
 {
     auto t = read_iris();
     std::cout << "Algorithm " << t.size() << std::endl;
-    vector<float> test{0, 1, 2, 3, 4};
+    std::vector<float> test{0, 1, 2, 3, 4};
     float *d_test;
     cudaMalloc((void **)&d_test, test.size());
     cudaMemcpy(d_test, test.data(), test.size(), cudaMemcpyHostToDevice);
-    std::cout << kernel_gpu<float>(d_test, test.size()) << std::endl;
+    kernel_gpu<<<500, 1>>>(d_test, test.size());
+    cudaDeviceSynchronize();
+    float answer;
+    cudaMemcpyFromSymbol(&answer, "kernel_gpu_answer", sizeof(answer), 0, cudaMemcpyFromDeviceToHost);
+    std::cout << answer << std::endl;
     cudaFree(d_test);
 }
