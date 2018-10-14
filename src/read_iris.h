f9fce0c1a6b5b5f456dd8743466ec92e2ea6b91a
persja40 <persja40@gmail.com>
2018-10-14 08:02:59 +0200

    read data fix

--- src/read_iris.h
+++ src/read_iris.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <string>
 #include <sstream>
+#include <iterator>
 
 std::vector<std::vector<float>> read_iris()
 {
@@ -13,11 +14,11 @@ std::vector<std::vector<float>> read_iris()
     std::ifstream fs("../data/iris.data", std::ios_base::in);
     for (std::string line; std::getline(fs, line);) //read stream line by line
     {
-        std::istringstream in(line);
-        float a, b, c, d;
-        fs >> a >> b >> c >> d;
-        std::vector<float> tmp{a, b, c, d};
-        std::cout << a << b << c << d << std::endl;
+        // std::cout << "line :" << line << std::endl;
+        std::istringstream iss(line);
+        std::vector<std::string> r((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
+        std::vector<float> tmp{std::stof(r[0]), std::stof(r[1]), std::stof(r[2]), std::stof(r[3])};
+        // std::cout << std::stof(r[0]) << std::stof(r[1]) << std::stof(r[2]) << std::stof(r[3]) << std::endl;
         result.push_back(tmp);
     }
     return result;
