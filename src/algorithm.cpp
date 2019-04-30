e83c28bfba1ca2a82e4983e2285bf2088b3e3300
persja40 <persja40@gmail.com>
2019-04-30 16:56:49 +0200

    up to cluster distance cpu

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -10,10 +10,10 @@
 #include "read_iris.h"
 using namespace std;
 
-float kernel(float x, int n)
+float kernel(float x)
 {
     // cout << x <<"\t"<< exp(-0.5 * x * x) / (2 * pow(M_PI, n * 0.5)) <<endl;
-    return exp(-0.5 * x * x) / (2 * pow(M_PI, n * 0.5));
+    return exp(-0.5 * x * x);
 }
 
 float g_h(vector<vector<float>> &data, float h, int kth)
@@ -47,109 +47,114 @@ float g_h(vector<vector<float>> &data, float h, int kth)
     return answer / (pow(m, 2) * pow(h, n)) + 2 / (m * pow(h, n)) / (pow(2 * M_PI, n * 0.5));
 }
 
-float estimator(float x, vector<vector<float>> &data, vector<float> &h, float *s = nullptr)
+// float estimator(float x, vector<vector<float>> &data, vector<float> &h, float *s = nullptr)
+// {
+//     float answer = 0;
+//     int m = data.size();
+//     int n = data[0].size();
+//     unsigned nr_threads = thread::hardware_concurrency();
+//     auto par_fun = [&](int nr) -> float {
+//         float r = 0;
+//         for (int i = nr; i < data.size(); i += nr_threads)
+//         {
+//             float k = 1;
+//             for (int j = 0; i < n; i++)
+//                 k *= kernel((x - data[i][j]) / h[j], n) / pow(h[j], n);
+//             r += k;
+//         }
+//         return r;
+//     };
+
+//     auto par_fun_s = [&](int nr) -> float {
+//         float r = 0;
+//         for (int i = nr; i < data.size(); i += nr_threads)
+//         {
+//             float k = 1;
+//             for (int j = 0; i < n; i++)
+//                 k *= kernel((x - data[i][j]) / (h[j] * s[i]), n) / pow(h[j], n);
+//             r += k / pow(s[i], n);
+//         }
+//         return r;
+//     };
+
+//     vector<future<float>> fut_handler{};
+
+//     for (int i = 0; i < nr_threads; i++)
+//         if (s == nullptr)
+//             fut_handler.push_back(std::async(std::launch::async, par_fun, i));
+//         else
+//             fut_handler.push_back(std::async(std::launch::async, par_fun_s, i));
+
+//     for (auto &e : fut_handler) //sync threads
+//         answer += e.get();
+
+//     return answer / m;
+// }
+
+float estimator2(vector<float> &x, vector<vector<float>> &data, vector<float> &h, float *s = nullptr)
 {
+    int m = data.size();
+    int n = data[0].size();
     float answer = 0;
-    int m = data.size();
-    int n = data[0].size();
-    unsigned nr_threads = thread::hardware_concurrency();
-    auto par_fun = [&](int nr) -> float {
-        float r = 0;
-        for (int i = nr; i < data.size(); i += nr_threads)
-        {
-            float k = 1;
-            for (int j = 0; i < n; i++)
-                k *= kernel((x - data[i][j]) / h[j], n) / pow(h[j], n);
-            r += k;
-        }
-        return r;
-    };
-
-    auto par_fun_s = [&](int nr) -> float {
-        float r = 0;
-        for (int i = nr; i < data.size(); i += nr_threads)
-        {
-            float k = 1;
-            for (int j = 0; i < n; i++)
-                k *= kernel((x - data[i][j]) / (h[j] * s[i]), n) / pow(h[j], n);
-            r += k / pow(s[i], n);
-        }
-        return r;
-    };
-
-    vector<future<float>> fut_handler{};
-
-    for (int i = 0; i < nr_threads; i++)
-        if (s == nullptr)
-            fut_handler.push_back(std::async(std::launch::async, par_fun, i));
-        else
-            fut_handler.push_back(std::async(std::launch::async, par_fun_s, i));
-
-    for (auto &e : fut_handler) //sync threads
-        answer += e.get();
-
-    return answer / m;
-}
-
-vector<float> estimator2(vector<float> &x, vector<vector<float>> &data, vector<float> &h, float *s = nullptr)
-{
-    int m = data.size();
-    int n = data[0].size();
-    vector<float> answer(n, 0);
     // for(auto e : answer)
     //     cout<<e<<"\t";
     // cout<<endl;
-    auto par_fun = [&]() -> void {
+    auto par_fun = [&]() -> float {
+        float r = 0;
         for (int i = 0; i < data.size(); i++)
-            for (int j = 0; i < n; i++){
-                cout<<"IN "<<x[j]<<"\t"<<data[i][j]<<"\t"<<h[j]<<endl;
-                answer[i] += kernel((x[j] - data[i][j]) / h[j], n);
+        {
+            float k = 1;
+            for (int j = 0; j < n; j++)
+            {
+                // cout << "IN " << x[j] << "\t" << data[i][j] << "\t" << h[j] << endl;
+                k *= kernel((x[j] - data[i][j]) / h[j]);
                 // cout << kernel((x[j] - data[i][j]) / h[j], n) << endl;
             }
+            r += k / powf(2 * M_PI, n * 0.5);
+        }
+        return r;
     };
 
-    auto par_fun_s = [&]() -> void {
+    auto par_fun_s = [&]() -> float {
+        float r = 0;
         for (int i = 0; i < data.size(); i++)
-            for (int j = 0; i < n; i++){
-                answer[i] += kernel((x[j] - data[i][j]) / (h[j] * s[i]), n) / pow(s[i], n);
+        {
+            float k = 1;
+            for (int j = 0; j < n; j++)
+            {
+                k *= kernel((x[j] - data[i][j]) / (h[j] * s[i]));
                 // cout<< kernel((x[j] - data[i][j]) / (h[j] * s[i]), n) / pow(s[i], n) <<endl;
             }
+            r += k / powf(2 * M_PI, n * 0.5) / pow(s[i], n);
+        }
+        return r;
     };
 
     if (s == nullptr)
-        par_fun();
+        answer = par_fun();
     else
-        par_fun_s();
+        answer = par_fun_s();
 
     // for(auto e : answer)
     //     cout<<e<<"\t";
     // cout<<endl;
 
     for (int i = 0; i < n; i++)
-        answer[i] /= m * pow(h[i], n);
-
-    // for(auto e : answer)
-    //     cout<<e<<"\t";
-    // cout<<endl<<endl;
-
-    return answer;
+        answer *= 1.0 / h[i];
+    return answer / m;
 }
 
 vector<float> si_array(vector<vector<float>> &data, vector<float> &h, float c)
 {
-    vector<float> answer{}; //first f*
     int m = data.size();
     int n = data[0].size();
-    answer.reserve(m);
+    vector<float> answer(m, 0); //first f*
     unsigned nr_threads = thread::hardware_concurrency();
     auto par_fun = [&](int nr) -> void {
         float r = 0;
         for (int i = nr; i < data.size(); i += nr_threads)
         {
-            float k = 1;
-            for (int j = 0; i < n; i++)
-                k *= kernel(data[i][j], n) / pow(h[j], n);
-            answer[i] = k / m;
+            answer[i] = estimator2(data[i], data, h);
         }
     };
 
@@ -159,9 +164,10 @@ vector<float> si_array(vector<vector<float>> &data, vector<float> &h, float c)
     for (auto &e : fut_handler) //sync threads
         e.wait();
 
-    float si_avg = accumulate(begin(answer), end(answer), 1, [&](float r, float x) {
-        return pow(x, 1.0 / m);
+    float si_avg = accumulate(begin(answer), end(answer), 0, [&](float r, float x) {
+        return r + log(x);
     });
+    si_avg = expf(si_avg / m);
 
     //now si
     for (auto &e : answer)
@@ -192,7 +198,7 @@ vector<float> goldenRatio(vector<vector<float>> &data, float a_u = 0.0001, float
             f1 = g_h(data, l1, nth);
             f2 = g_h(data, l2, nth);
 
-            cout<<"l1 "<<l1<<"\tf1 "<<f1<<"\tl2 "<<l2<<"\tf2 "<<f2<<" "<<nth<<endl;
+            // cout << "l1 " << l1 << "\tf1 " << f1 << "\tl2 " << l2 << "\tf2 " << f2 << " " << nth << endl;
 
             if (f2 > f1)
                 b = l2;
@@ -235,7 +241,10 @@ void step(vector<vector<float>> &data, vector<float> &h, float dx = 0.01)
 {
     int n = data[0].size();
     auto s = si_array(data, h, 0.5);
-    unsigned nr_threads = 1;//thread::hardware_concurrency();
+    // for(auto e:s)
+    //     cout<<e<<"\t";
+    //     cout<<endl;
+    unsigned nr_threads = thread::hardware_concurrency();
     vector<float> b{};
     for (auto &e : h)
         b.push_back(pow(e, 2) / (n + 2));
@@ -244,15 +253,15 @@ void step(vector<vector<float>> &data, vector<float> &h, float dx = 0.01)
     auto par_fun = [&](int nr) -> void {
         for (int i = nr; i < data.size(); i += nr_threads)
         {
-            auto r = estimator2(data[i], data, h);
+            auto r = estimator2(data[i], data, h, s.data());
             auto data_dx = data[i];
             for (auto &e : data_dx)
                 e += dx;
-            auto r_dx = estimator2(data_dx, data, h);
+            auto r_dx = estimator2(data_dx, data, h, s.data());
             for (int j = 0; j < n; j++)
             {
                 // cout << r[j] << "\t";
-                new_data[i][j] += b[j] * (r_dx[j] - r[j]) / dx / r[j];
+                new_data[i][j] += b[j] * (r_dx - r) / dx / r;
             }
         }
     };
@@ -271,15 +280,112 @@ void stopCondition(vector<vector<float>> &data, std::vector<float> &h, float alp
     float d0 = distance(data);
     float dk_m1 = numeric_limits<float>::max();
     float dk = d0;
-    cout << d0 << "\t" << dk_m1 << "\t" << dk << "\t" << fabs(dk_m1 - dk) << endl;
+    //cout << d0 << "\t" << dk_m1 << "\t" << dk << "\t" << fabs(dk_m1 - dk) << endl;
     while (fabs(dk - dk_m1) > alpha * d0)
     {
         step(data, h);
         dk_m1 = dk;
         dk = distance(data);
-        cout << d0 << "\t" << dk_m1 << "\t" << dk << "\t" << fabs(dk_m1 - dk) << endl;
+        //cout << d0 << "\t" << dk_m1 << "\t" << dk << "\t" << fabs(dk_m1 - dk) << endl;
     }
 }
+
+vector<float> distanceArray(vector<vector<float>> &data){
+    int m = data.size();
+    int n = data[0].size();
+    vector<float> result{};
+    result.reserve(m*(m-1)/2);
+    unsigned nr_threads = thread::hardware_concurrency();
+    auto fun_dist = [&](int nr) -> vector<float>{
+        vector<float> r{};
+        r.reserve(m*(m-1)/2/nr_threads);
+        for(int i=nr; i<m; i+=nr_threads){
+            for(int j=i+1;j<m; j++){
+                float v = 0;
+                for(int k=0; k<n; k++)
+                    v+= pow(data[i][k] - data[j][k], 2);
+                r.push_back(sqrt(v));
+            }
+        }
+        return r;
+    };
+
+    vector<future<vector<float>>> fut_handler{};
+    for (int i = 0; i < nr_threads; i++)
+        fut_handler.push_back(std::async(std::launch::async, fun_dist, i));
+    for (auto &e : fut_handler){
+        auto w = e.get();
+        result.insert(result.end(), w.begin(), w.end());
+    }
+    return result;
+}
+
+float maxDistance(vector<float> &data){
+    float result = numeric_limits<float>::min();
+    unsigned nr_threads = thread::hardware_concurrency();
+    auto fun_max = [&](int nr) -> float{
+        float r = numeric_limits<float>::min();
+        for(int i=nr; i<data.size(); i+=nr_threads)
+            r = max(r, data[i]);
+        return r;
+    };
+    vector<future<float>> fut_handler{};
+    for (int i = 0; i < nr_threads; i++)
+        fut_handler.push_back(std::async(std::launch::async, fun_max, i));
+    for (auto &e : fut_handler)
+        result = max(result, e.get());
+    return result;
+}
+
+float sigma(vector<float> &data){
+    int m = data.size();
+    float result = 0; //EX
+    unsigned nr_threads = thread::hardware_concurrency();
+    auto fun_avg = [&](int nr) -> float{
+        float r = 0;
+        for(int i=nr; i<m; i+=nr_threads)
+            r +=data[i]/m;
+        return r;
+    };
+    vector<future<float>> fut_handler{};
+    for (int i = 0; i < nr_threads; i++)
+        fut_handler.push_back(std::async(std::launch::async, fun_avg, i));
+    for (auto &e : fut_handler)
+        result += e.get();
+    fut_handler.clear();
+    auto fun_sigma_sum = [&](int nr, float EX) -> float{
+        float r = 0;
+        for(int i=nr; i<m; i+=nr_threads)
+            r += pow(data[i] - EX, 2)/(m-1);
+        return r;
+    };
+    for (int i = 0; i < nr_threads; i++)
+        fut_handler.push_back(std::async(std::launch::async, fun_sigma_sum, i, result));
+    result = 0; //sigma_d
+    for (auto &e : fut_handler)
+        result += e.get();
+    return sqrt(result);
+}
+
+bool distanceCondition(vector<float> &data, vector<float> &si, vector<float> &h){
+    return false;
+}
+
+float clusterDistance(vector<vector<float>> &data){
+    auto data_dist = distanceArray(data);
+    cout<<"Dist arr size: "<<data_dist.size()<<endl;
+    float D = maxDistance(data_dist);
+    float sigma_d = sigma(data_dist);
+    cout<<"Sigma_d: "<<sigma_d<<endl;
+    // auto h = goldenRatio(data_dist);
+
+    for(float xd = 0.01*sigma_d; xd < static_cast<int>(100*D-1)*0.01*sigma_d; xd+=0.01*sigma_d){
+        return 0;
+    }
+    return -1;
+}
+
+
 
 int main(int argc, char **argv)
 {
@@ -296,5 +402,6 @@ int main(int argc, char **argv)
     for (const auto &e : h)
         std::cout << e << std::endl;
     stopCondition(t, h);
+    clusterDistance(t);
     // std::cout << "CPU: " << g_h_sum_cpu(t, 1.0) << std::endl;
 }
